502a645624a2ca922091d067255d3a1a49140fa2
sramisetty1 <sramisetty1@miraclesoft.com>
2025-02-13 15:20:16 +0530

    second commit

--- basicreact/src/App.jsx
+++ basicreact/src/App.jsx
@@ -1,10 +1,25 @@
-
-
+import './index.css' 
+import { useState } from "react"
 function App() {
-
+  const [counter,setCounter]=useState(10)
+  const adding=()=>
+  {
+    setCounter(counter+1);
+  }
+  const sub=()=>
+  {
+    setCounter((prev)=>prev-1); // btching --> recoilation
+    setCounter((prev)=>prev-1);
+    setCounter((prev)=>prev-1);
+    setCounter((prev)=>prev-1);
+  }
   return (
     <>
-      <h1>Hai Siva</h1>
+      <div>
+        <h1 className="flex text-3xl bg-green-500 underline p-4 rounded-md">Counter: {counter}</h1>
+        <button onClick={adding}>ADD</button>{" "}
+        <button onClick={sub}>SUB</button>
+      </div>
     </>
   )
 }
